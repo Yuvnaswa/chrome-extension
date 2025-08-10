132b572d28f6176cc5a31b67f306aa56022bbd28
Yuvnaswa <singhyuvnaswa9@gmail.com>
2025-08-10 22:54:48 +0530

    added the local storage code

--- main.js
+++ main.js
@@ -4,10 +4,19 @@ const inputEl = document.getElementById("input-el");
 const saveInputEl = document.getElementById("saveInput-el");
 const ulEl = document.getElementById("myList");
 
+//Check for the added leads and show if already added
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderList();
+}
+
 saveInputEl.addEventListener("click", () => {
   const inputValue = inputEl.value;
   if (inputValue !== "") {
     myLeads.push(inputEl.value);
+    //storing each lead to the localStorage on click
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
     inputEl.value = "";
     renderList();
   }
